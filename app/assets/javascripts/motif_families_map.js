8ee33106b8bbd61b9eccfa4f7880c1d3c9d0be57
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2018-11-19 14:50:12 +0300

    update motif family tree to work with new d3.js

--- app/assets/javascripts/motif_families_map.js
+++ app/assets/javascripts/motif_families_map.js
@@ -5,18 +5,19 @@ jQuery(function(){
       height = 700,
       tree_url = HocomocoDB.app_prefix + "tree_level_2.json";
 
-  var tree = d3.layout.tree()
+  var treeLayout = d3.tree()
       .size([height, width - 300])
       .separation(function(a,b){
-        if (Math.max( Math.sqrt(a.total_tfs), Math.sqrt(b.total_tfs) ) > 10) {
+        if (Math.max( Math.sqrt(a.data.total_tfs), Math.sqrt(b.data.total_tfs) ) > 10) {
           return 4;
         } else {
           return (a.parent == b.parent) ? 1.5 : 2;
         }
       });
 
-  var diagonal = d3.svg.diagonal()
-      .projection(function(d) { return [d.y, d.x]; });
+  var diagonal = d3.linkHorizontal()
+    .x(function(d) { return d.y; })
+    .y(function(d) { return d.x; });
 
   var svg = d3.select("#motif_families_map").append("svg")
       .attr("width", width)
@@ -24,11 +25,11 @@ jQuery(function(){
     .append("g")
       .attr("transform", "translate(40,0)");
 
-  d3.json(tree_url, function(error, root) {
-    if (error) throw error;
-
-    var nodes = tree.nodes(root),
-        links = tree.links(nodes);
+  d3.json(tree_url).then(function(data) {
+    var root = d3.hierarchy(data);
+    var nodes = root.descendants(),
+        links = root.links();
+    treeLayout(root);
 
     var link = svg.selectAll(".link")
         .data(links)
@@ -40,7 +41,7 @@ jQuery(function(){
         .data(nodes)
       .enter()
         .append("svg:a")
-        .attr('xlink:href', function(d){return (HocomocoDB.app_prefix + d.url).replace(/\/+/g,'/');})
+        .attr('xlink:href', function(d){return (HocomocoDB.app_prefix + d.data.url).replace(/\/+/g,'/');})
         .append("g")
           .attr("class", "node")
           .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
@@ -52,16 +53,16 @@ jQuery(function(){
     node
       .on("mouseover", function(d) {
         var info_text = '';
-        if (d.name.length > 0) {
-          var family_name = '<span class="family_name">' + d.name + '{' + d.family_id + '}</span>';
-          // info_text = '<a href="http://tfclass.bioinf.med.uni-goettingen.de/tfclass?uniprot=' + d.family_id + '">' + family_name + '</a>' +
+        if (d.data.name.length > 0) {
+          var family_name = '<span class="family_name">' + d.data.name + '{' + d.data.family_id + '}</span>';
+          // info_text = '<a href="http://tfclass.bioinf.med.uni-goettingen.de/?tfclass=' + d.data.family_id + '">' + family_name + '</a>' +
           info_text = family_name;
           info_text += '<br/>';
         }
         info_text += '<br/>' + 'HOCOMOCO / Total TFs:<br/>' +
-                  '<span class="covered_tfs">' + d.covered_tfs + '</span>' + ' / ' + '<span class="total_tfs">' + d.total_tfs + '</span>';
-        if (d.comment) {
-          info_text += '<br/>' + '<i>' + d.comment + '</i>';
+                  '<span class="covered_tfs">' + d.data.covered_tfs + '</span>' + ' / ' + '<span class="total_tfs">' + d.data.total_tfs + '</span>';
+        if (d.data.comment) {
+          info_text += '<br/>' + '<i>' + d.data.comment + '</i>';
         }
         div.transition()
             .duration(200)
@@ -77,21 +78,21 @@ jQuery(function(){
       });
 
     node.append("circle")
-        .attr("r", function(d) { return Math.pow(d.total_tfs, 0.5) + 1; } ) // 1 is an addition to make circles visually more comparable
+        .attr("r", function(d) { return Math.pow(d.data.total_tfs, 0.5) + 1; } ) // 1 is an addition to make circles visually more comparable
         .attr("class", "total_tfs");
 
     node.append("circle")
-        .attr("r", function(d) { return Math.pow(d.covered_tfs, 0.5); } ) // we don't add 1 to inner circle
+        .attr("r", function(d) { return Math.pow(d.data.covered_tfs, 0.5); } ) // we don't add 1 to inner circle
         .attr("class", "covered_tfs");
 
     node.append("text")
         .attr("dx", function(d) {
-          var radius = Math.pow(d.total_tfs, 0.5) + 1;
+          var radius = Math.pow(d.data.total_tfs, 0.5) + 1;
           return d.children ? -(radius + 5) : (radius + 5); }
         )
         .attr("dy", 3)
         .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-        .text(function(d) { return d.name; });
+        .text(function(d) { return d.data.name; });
   });
 
   d3.select(self.frameElement).style("height", height + "px");
