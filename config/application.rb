496497720fbc6f49412b8dd94040809a33530f70
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-02-12 18:17:17 +0300

    app:update 5.1

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 HOCOMOCO_VERSION_NUMBER = 11
 HOCOMOCO_VERSION = "hocomoco#{HOCOMOCO_VERSION_NUMBER}"
+
 module HocomocoSite
   class Application < Rails::Application
     config.before_configuration do
@@ -24,6 +16,10 @@ module HocomocoSite
       next  unless File.exists?(env_file)
       YAML.load_file(env_file).each{|k,v| ENV[k] ||= v }
     end
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
