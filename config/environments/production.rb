3123d450911be9cd419dc5ff7329e2bf9b5f85b3
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-02-28 23:37:52 +0300

    use https links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_controller.default_url_options ||= {}
+  config.action_controller.default_url_options[:protocol] = 'https'
 end
