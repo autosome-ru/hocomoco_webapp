826fd0438822c3058335741b5719fc817c9ec9d4
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-10-16 19:32:26 +0300

    add nofollow for download single motif links

--- app/decorators/motif_decorator.rb
+++ app/decorators/motif_decorator.rb
@@ -66,16 +66,16 @@ class MotifDecorator < ApplicationDecorator
       object.revcomp_large_logo_url,
     )
   end
-  def download_pcm; helpers.link_to("#{name}.pcm", object.pcm_url); end
-  def download_pwm; helpers.link_to("#{name}.pwm", object.pwm_url); end
-  def download_pfm; helpers.link_to("#{name}.pfm", object.pfm_url); end
-  def download_alignment; helpers.link_to("#{name}.words.tsv", object.alignment_url); end
-  def download_precalculated_thresholds; helpers.link_to("#{name}.thr", object.precalculated_thresholds_url); end
+  def download_pcm; helpers.link_to("#{name}.pcm", object.pcm_url, rel: 'nofollow'); end
+  def download_pwm; helpers.link_to("#{name}.pwm", object.pwm_url, rel: 'nofollow'); end
+  def download_pfm; helpers.link_to("#{name}.pfm", object.pfm_url, rel: 'nofollow'); end
+  def download_alignment; helpers.link_to("#{name}.words.tsv", object.alignment_url, rel: 'nofollow'); end
+  def download_precalculated_thresholds; helpers.link_to("#{name}.thr", object.precalculated_thresholds_url, rel: 'nofollow'); end
 
-  def download_jaspar_motif; helpers.link_to("#{name}_jaspar_format.txt", object.jaspar_url); end
-  def download_meme_motif; helpers.link_to("#{name}_meme_format.meme", object.meme_url); end
-  def download_transfac_motif; helpers.link_to("#{name}_transfac_format.txt", object.transfac_url); end
-  def download_homer_motif(pvalue:); helpers.link_to("#{name}_homer_format_#{pvalue}.txt", object.homer_url(pvalue: pvalue)); end
+  def download_jaspar_motif; helpers.link_to("#{name}_jaspar_format.txt", object.jaspar_url, rel: 'nofollow'); end
+  def download_meme_motif; helpers.link_to("#{name}_meme_format.meme", object.meme_url, rel: 'nofollow'); end
+  def download_transfac_motif; helpers.link_to("#{name}_transfac_format.txt", object.transfac_url, rel: 'nofollow'); end
+  def download_homer_motif(pvalue:); helpers.link_to("#{name}_homer_format_#{pvalue}.txt", object.homer_url(pvalue: pvalue), rel: 'nofollow'); end
 
   def format_matrix_as_table(matrix, round: nil)
     nucleotides = ['A', 'C', 'G', 'T']
