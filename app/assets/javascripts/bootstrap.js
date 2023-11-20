71b5accef6360a137d4361c1c990d00be477636c
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-11-20 19:13:56 +0300

    now tabs change url location and allows one to directly choose certain tab

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,29 @@
 $(function() {
   $("a[rel~=popover], .has-popover").popover()
   $("a[rel~=tooltip], .has-tooltip").tooltip()
+
+  $(document).on('show.bs.tab', '[data-toggle="tab"], [data-toggle="pill"], [data-toggle="list"]', function(event) {
+    let targetAnchor = $(event.target).attr("href")
+    document.location.href = targetAnchor;
+  });
+
+  let switchToTab = function(url) {
+    let urlHashPos = url.indexOf('#');
+    if (urlHashPos != -1) {
+      let urlHash = url.slice(urlHashPos);
+      $(`a[data-toggle="tab"][href="${urlHash}"]`).trigger('click');
+    } else {
+      let defaultTab = $('a.default-tab[data-toggle="tab"]')
+      if (defaultTab.attr('href')) {
+        document.location.href = defaultTab.attr('href');
+      }
+    }
+  };
+
+  switchToTab(document.location.href);
+
+  window.addEventListener('hashchange', function(event){
+    event.preventDefault();
+    switchToTab(event.newURL);
+  });
 });
